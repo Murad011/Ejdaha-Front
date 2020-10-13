e57be8e99bef41f3a4a623add9ae629989b3ea33
Murad011 <muradaydin122@gmail.com>
2020-10-13 17:39:24 +0400

    Project Created

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+
+
 
 new Vue({
   render: h => h(App),
