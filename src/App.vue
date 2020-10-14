520faf5585c2bfdb52d5e6c5de270f1c7a16b641
Murad011 <muradaydin122@gmail.com>
2020-10-14 16:30:27 +0400

    Header created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
    
-    <navbar />
+    <Navbar />
+    <Header />
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Header
   }
 }
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,21 +8,13 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <b-nav-item href="#">Free</b-nav-item>
+        <b-nav-item href="#" >Free</b-nav-item>
       </b-navbar-nav>
-
-      <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+        <b-nav-form> 
         </b-nav-form>
-
-     
-
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
+      <b-nav-item-dropdown right>
           <template v-slot:button-content>
             <em>User</em>
           </template>
@@ -43,7 +35,6 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 
 </style>
