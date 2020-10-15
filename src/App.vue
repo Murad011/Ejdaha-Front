be201520748776bc2c5539953bdfb1dfb9b80040
Murad198 <azdemy.az@gmail.com>
2020-10-15 14:39:02 +0400

    all flesh switched

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
    
-    <navbar />
+    <Navbar />
+    <Map/>
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
+import Map from './components/Map.vue'
 
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Map
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import 'leaflet/dist/leaflet.css';
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 import 'bootstrap/dist/css/bootstrap.css'
@@ -5,6 +6,16 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
+import { Icon } from 'leaflet';
+
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+
 
 Vue.config.productionTip = false
 
