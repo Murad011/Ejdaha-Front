1972ffaa78e691332ef64a410ac3a0d1959e1e39
Murad011 <muradaydin122@gmail.com>
2020-10-16 14:01:44 +0400

    Header and Staticties page created

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,36 @@
   <div id="app">
    
     <Navbar />
-    <Header />
+    <router-view></router-view>
+      
+     
+      
+      
+   
   </div>
 </template>
 
 <script>
 import Navbar from './components/Navbar.vue'
-import Header from './components/Header.vue'
+
+
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    Header
-  }
+   
+    
+    
+  },
+
 }
 </script>
 
 <style>
-
+#app {
+  font-family: 'Commissioner', sans-serif;
+  /* text-align: center;
+  margin-top: 30px; */
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vue from 'vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
+Vue.use(VueRouter)
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
-
+import Header from './components/Header.vue'
+import Staticties from './components/Staticties.vue'
+const router = new VueRouter({
+  routes: [
+    {path: '/', component: Header },
+    {path: '/staticties', component: Staticties}
+  ]
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
